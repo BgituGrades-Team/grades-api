5d88ba4e3dd24f379a49355ccf9e67569fae50b9
Damsmh <shmychkov210605@gmail.com>
2026-03-05 12:10:48 +0300

    fix migration

--- BgituGrades/Migrations/20260305084926_UpdateKeyEntity.Designer.cs
+++ BgituGrades/Migrations/20260305084926_UpdateKeyEntity.Designer.cs
@@ -9,7 +9,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
-namespace BgutuGrades.Migrations
+namespace BgituGrades.Migrations
 {
     [DbContext(typeof(AppDbContext))]
     [Migration("20260305084926_UpdateKeyEntity")]
